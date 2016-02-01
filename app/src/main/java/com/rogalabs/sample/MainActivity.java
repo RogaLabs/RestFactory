2b1f9cc0be6df73a402176ce8f74b1f3c3ca0aac
Felipe Oliveira <felipe.oliveiradev@gmail.com>
2016-02-01 13:49:14 -0300

    sample

--- app/src/main/java/com/rogalabs/sample/MainActivity.java
+++ app/src/main/java/com/rogalabs/sample/MainActivity.java
@@ -1,13 +1,43 @@
 package com.rogalabs.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.gson.JsonElement;
+import com.rogalabs.apifactory.Rest;
+import com.rogalabs.apifactory.RestFactory;
+
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Rest
+    MockApi mockApi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RestFactory.make(this);
+
+
+        mockApi.getX().enqueue(new Callback<JsonElement>() {
+            @Override
+            public void onResponse(Response<JsonElement> response) {
+                Log.d("SUCCESS", response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.d("FAIL", t.getMessage()
+                );
+            }
+        });
+
+
     }
 }
